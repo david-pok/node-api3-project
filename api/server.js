a86dc55ab4110f8dcf5f6e5e596a3f8779288f0a
David <david.pok@gmail.com>
2021-05-13 20:57:38 -0700

    setup server and some middleware

--- api/server.js
+++ api/server.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
 
 const server = express();
-
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
+//morgan is a commercial middleware that creates a console log
+//that tells us about a request object
+server.use(morgan("dev"));
+
+//this is a custom middleware that does pretty much
+//what morgan does
+server.use((req, res, next) => {
+  console.log("the path is: ", req.path);
+  next();
+});
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
+  // throw new Error("DISASTER ERROR"); //use this to test error catches
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//error handling middleware that will catch errors that come
+//before it does
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message, //dev only
+    stack: err.stack, //dev only
+    custom: "something went terrible", //simple error message for production
+  });
 });
 
 module.exports = server;
